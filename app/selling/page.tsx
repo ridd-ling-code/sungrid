04fb6ca9847575077bd3723f18dd70722df5ca93
Gaurangi Goenka <99355618+ridd-ling-code@users.noreply.github.com>
2024-10-26 03:53:33 +0530

    Update page.tsx

--- app/selling/page.tsx
+++ app/selling/page.tsx
@@ -1,4 +1,4 @@
-import Layout from '@/app/layout.tsx';
+import Layout from '@/app/layout';
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 
